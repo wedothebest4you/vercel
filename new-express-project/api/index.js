91661b18f4cb99c6d889a5332d5a86755cc5739d
wedothebest4you <wedothebest4you@gmail.com>
2024-08-10 11:41:44 +0530

    vercel deploy

--- new-express-project/api/index.js
+++ new-express-project/api/index.js
@@ -1,23 +1,12 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(cors());
 
-app.use(cookieParser());
-
-app.get('/cookie', (req, res) => {
-  let response;
-
-  if (req.cookies?.somecookie) {
-    response = 'Same cookie: A cookie received and the same sent to client';
-  } else {
-    res.cookie('somecookie', 'cookie text');
-    response = 'New cookie: A new cookie created and sent to the client';
-  }
-
-  res.send(response);
+app.get('/', (req, res) => {
+  res.send('done');
 });
 
 app.listen(4000, () => {
